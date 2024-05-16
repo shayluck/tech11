084d06425ec218b9543787c144342ed7d333f960
shayluck <shay.luck@my.jcu.edu.au>
2024-05-16 11:07:35 +1000

    try3

---
+++ tech1git.R
@@ -0,0 +1,6 @@
+use_git_config(user.name = "<shayluck>", user.email = "<shay.luck@my.jcu.edu.au>")
+
+install.packages("usethis")
+library(usethis)
+use_git_config(user.name = "shayluck", user.email = "shay.luck@my.jcu.edu.au")
+
